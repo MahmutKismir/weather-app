f9011db62bc5b998c1ac4013b749eef2a9e7ebe9
Mahmut Kismir <103948581+MahmutKismir@users.noreply.github.com>
2023-06-14 23:19:34 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ import "./App.css";
 function App() {
   
   return (
-    <div className="flex justify-center items-center w-full h-screen bg-neutral-300">
-      <div className="flex flex-col items-center w-2/3 h-4/5 rounded-md py-1 px-8 bg-[url('/image/maidenstower.jpeg')] bg-cover bg-no-repeat ">
+    <div className="flex justify-center items-center w-full h-screen bg-neutral-700">
+      <div className="
+      flex 
+      flex-col 
+      items-center 
+      w-2/3 
+      h-4/5 
+      rounded-md 
+      py-4 
+      px-10 
+      xl:bg-[url('/image/maidenstower.jpeg')] 
+      bg-cover
+      bg-center 
+      bg-no-repeat 
+      md:bg-[url('/image/tower.jpeg')] 
+      sm:bg-[url('/image/tower.jpeg')]
+      min 
+      ">
         <Header title="Weather App" />
         <SearchContent />
       </div>
